8563874d91db43723e8a5c8fc29dd467aa8dc789
maofaming <229894116@qq.com>
2020-08-12 14:15:36 +0800

    资讯详情页面增加倒计时

---
+++ android/src/main/java/cn/bloomad/util/DensityUtils.java
@@ -0,0 +1,91 @@
+package cn.bloomad.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class DensityUtils {
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 将sp转换为px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取屏幕高度（像素）
+     */
+    public static int getScreenHeight(Context context) {
+        try {
+            DisplayMetrics dm = new DisplayMetrics();
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(dm);
+            return dm.heightPixels;
+        } catch (Exception e) {
+            //
+        }
+        return 0;
+    }
+
+    public static int getScreenWidth(Context context) {
+        try {
+            DisplayMetrics dm = new DisplayMetrics();
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(dm);
+            return dm.widthPixels;
+        } catch (Exception e) {
+            //
+        }
+        return 0;
+    }
+
+    public static float getScreenDensity(Context context) {
+        try {
+            DisplayMetrics dm = new DisplayMetrics();
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(dm);
+            return dm.density;
+        } catch (Exception e) {
+            //
+        }
+        return 2;
+    }
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusHeight(Context context) {
+        int statusBarHeight = -1;
+        // 获取status_bar_height资源的ID
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            // 根据资源ID获取响应的尺寸值
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        } else {
+            statusBarHeight = dip2px(context, 38);
+        }
+        return statusBarHeight;
+    }
+}
---
+++ android/src/main/java/cn/bloomad/widget/CountdownView.java
@@ -0,0 +1,113 @@
+package cn.bloomad.widget;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.linkin.newssdk.AbstractCountdownView;
+
+import cn.bloomad.R;
+
+public class CountdownView extends AbstractCountdownView {
+
+    private TextView mTvReward;
+    private ImageView mIvRedPack;
+    private DonutProgress mProgress;
+
+    public CountdownView(Context context) {
+        super(context);
+        // 请按照设计修改布局
+        View view = View.inflate(context, R.layout.view_countdown, this);
+        mTvReward = view.findViewById(R.id.tv_reward);
+        mIvRedPack = view.findViewById(R.id.iv_red_pack);
+        mProgress = view.findViewById(R.id.progress);
+        mProgress.setMax(1);
+    }
+
+    @Override
+    public void onStart(Object rewardData) {
+        mProgress.setProgress(0);
+    }
+
+    @Override
+    public void onProgressUpdate(float progress, int totalSeconds) {
+        // progress 在0到1之间
+        mProgress.setProgress(progress);
+    }
+
+    @Override
+    public void onEnd(boolean rewardSuccess, Object rewardData) {
+        if (!rewardSuccess) {
+            // 奖励失败
+            removeFromParent();
+            return;
+        }
+
+        mTvReward.setText("+" + rewardData);
+
+        // 自定义奖励动画，请按照设计修改代码
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(makeRewardAnimator(), makeRedPackAnimator(), makeAlphaAnimator());
+
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mTvReward.setVisibility(View.VISIBLE);
+                mIvRedPack.setScaleX(0.9f);
+                mIvRedPack.setScaleY(0.9f);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                removeFromParent();
+
+                CountdownView.this.setAlpha(1f);
+                mTvReward.setVisibility(View.GONE);
+                mIvRedPack.setRotation(0f);
+                mIvRedPack.setScaleX(1f);
+                mIvRedPack.setScaleY(1f);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+
+        animatorSet.start();
+    }
+
+    private Animator makeRewardAnimator() {
+        PropertyValuesHolder pvhTranslationY = PropertyValuesHolder.ofFloat("translationY", 0.0f, -5f);
+        PropertyValuesHolder pvhAlpha = PropertyValuesHolder.ofFloat("alpha", 0.1f, 1f);
+        PropertyValuesHolder pvhTextSize = PropertyValuesHolder.ofFloat("textSize", 10f, 30f);
+
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(mTvReward, pvhTranslationY, pvhAlpha, pvhTextSize);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.setDuration(1000);
+        return animator;
+    }
+
+    private Animator makeRedPackAnimator() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mIvRedPack, "rotation", -20f, 20f);
+        animator.setDuration(100).setRepeatCount(10);
+        return animator;
+    }
+
+    private Animator makeAlphaAnimator() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(this, "alpha", 1f, 0f);
+        animator.setInterpolator(new DecelerateInterpolator());
+        animator.setStartDelay(800);
+        animator.setDuration(200);
+        return animator;
+    }
+}
